57943437bedce058faa9339dd160584e5cf9fdae
antonio98s <antoniosostilio523@gmail.com>
2020-03-23 11:05:20 -0400

    Updates

---
+++ precache-manifest.bafe5b208bff393d6b83b75889e77086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0017d21b27e8b8e34bdf010769a41a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1cef221bb4ae6aed593f",
+    "url": "/monsters-rolodex/static/css/main.03121f44.chunk.css"
+  },
+  {
+    "revision": "9618ac727f25fb5f27cc",
+    "url": "/monsters-rolodex/static/js/2.d3b2774f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.d3b2774f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cef221bb4ae6aed593f",
+    "url": "/monsters-rolodex/static/js/main.7adcf5f6.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
